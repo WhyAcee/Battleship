cf7eb64091c7b4faf7b858e2690e68299c14b34c
Marlon <marlonwason100@gmail.com>
2023-10-03 13:39:49 -0400

    Add title Screen functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import './style.css'
 import Gameboard from './gameboard'
 import Ship from './ship'; 
+import Player from './player';
 
-const gameboard = new Gameboard(10, 10)
-const ship = new Ship(3);
-gameboard.placeShip(ship, 2, 3, 'horizontal');
-const result = gameboard.receiveAttack(2, 3);
-console.log(result)
+const content = document.querySelector('.content')
+const titleScreen = document.querySelector('.title-screen')
+const inputName = document.querySelector('#name')
+const startBtn = document.querySelector('#start-btn')
+
+function CreateTitleScreen() {
+    titleScreen.style.display = 'flex'
+}
  
+
+startBtn.addEventListener('click', () => {
+    titleScreen.style.display = 'none'
+    if (inputName.value === '') {
+        inputName.value = 'Human'
+    }
+    console.log(inputName.value)
+    
+    gameLoop()
+}) 
