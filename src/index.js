b9f685bccbbfd6777012a514afb7ff76204aac03
Marlon <marlonwason100@gmail.com>
2023-10-08 18:36:24 -0400

    Add hover functionality

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const computerGameboard = new Gameboard(10, 10, computerGameboardContainer)
 
 const shipsToPlace = [playerGameboard.carrier, playerGameboard.battleship, playerGameboard.cruiser, playerGameboard.submarine, playerGameboard.destroyer]
 let currentShipIndex = 0;
+let orientation = 'vertical' 
 
 function CreateTitleScreen() {
     message.style.display = 'none'
@@ -69,7 +70,6 @@ function gameLoop(name) {
 
     message.textContent = `Place your ships ${name}.`
 
-    //playerGameboard.autoPlaceShips()
     computerGameboard.autoPlaceShips()
     
 
@@ -85,17 +85,22 @@ const clickHandler = (event) => {
     const col = parseInt(cell.getAttribute('data-col'))
 
     const currentShip = shipsToPlace[currentShipIndex]
-    message.textContent = `Placing ${currentShip.name} (Length: ${currentShip.length})`;
+    message.textContent = `Place your ${currentShip.name} (Length: ${currentShip.length})`;
     
-    const placed = playerGameboard.placeShip(currentShip, row, col, 'horizontal')
+    const placed = playerGameboard.placeShip(currentShip, row, col, orientation)
 
     if (placed) {
         playerGameboard.displayShipOnGrid(currentShip)
         currentShipIndex++;
+        const cellsToClear = playerGameboardContainer.querySelectorAll('.highlighted-cell');
+        cellsToClear.forEach((cell) => {
+            cell.classList.remove('highlighted-cell');
+        }); 
 
         if(currentShipIndex === shipsToPlace.length) {
             message.textContent = 'Now Attack the Enemy board'
-            playerGameboardContainer.removeEventListener('click', clickHandler )
+            playerGameboardContainer.removeEventListener('click', clickHandler)
+            playerGameboardContainer.removeEventListener('mouseover', hoverHandler)
         } else {
             const nextShip = shipsToPlace[currentShipIndex];
             message.textContent = `Place your ${nextShip.name} (Length: ${nextShip.length})`;
@@ -103,6 +108,37 @@ const clickHandler = (event) => {
     } 
 }
 
+const hoverHandler = (event) => {
+    const cell = event.target
+    if (cell.hasAttribute('data-row') && cell.hasAttribute('data-col')) {
+        const row = parseInt(cell.getAttribute('data-row'));
+        const col = parseInt(cell.getAttribute('data-col'));
+
+        const currentShip = shipsToPlace[currentShipIndex];
+        const cellsToHighlight = []
+        if (orientation === 'horizontal' && col + currentShip.length <= playerGameboard.cols) {
+            for (let i = 0; i < currentShip.length; i++) {
+                cellsToHighlight.push(playerGameboardContainer.querySelector(`[data-row="${row}"][data-col="${col + i}"]`))
+            }
+        } else if (orientation === 'vertical' && row + currentShip.length <= playerGameboard.rows) {
+            for (let i = 0; i < currentShip.length; i++) {
+                cellsToHighlight.push(playerGameboardContainer.querySelector(`[data-row="${row + i}"][data-col="${col}"]`))
+            }
+        }
+
+        cellsToHighlight.forEach((highlightedCell) => {
+            highlightedCell.classList.add('highlighted-cell')
+        })
+    }
+}
+
+playerGameboardContainer.addEventListener('mouseout', (event) => {
+   
+    playerGameboardContainer.querySelectorAll('.highlighted-cell').forEach((cell) => {
+    cell.classList.remove('highlighted-cell');
+  });
+})
+
 playerGameboardContainer.addEventListener('click', clickHandler)
 
-
+playerGameboardContainer.addEventListener('mouseover', hoverHandler)
